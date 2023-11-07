bb27dba48197458dcc4e5ccb526581af1cf892e1
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-11-07 23:28:43 +0200

    add style Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
+import { NavLink } from 'react-router-dom';
+import Logo from '../../icon/logo.svg';
+
+import Search from '../../icon/search.svg';
+
 import styles from './Header.module.css';
 
 const Header = () => {
-  return <div className={styles.header}>HEADER</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__wraper}>
+        <div className={styles.logo}>
+          <NavLink to="/" className={styles.logo__link}>
+            <img src={Logo} />
+            <span className={styles.logo__text}>TechHaven</span>
+          </NavLink>
+        </div>
+
+        <div className={styles.header__nav}>
+          <div className={styles.header__inputLabel}>
+            <input
+              className={styles.header__input}
+              type="text"
+              autoComplete="off"
+              placeholder="Пошук товару..."
+            />
+
+            <button className={styles.header__inputBtn} type="submit">
+              <img width="27" height="23" src={Search} className={styles.header__inputIcon} />
+            </button>
+          </div>
+
+          <div className={styles.header__languageMenu}>
+            <select className={styles.header__languageSelect}>
+              <option value="ukr">UA</option>
+              <option value="eng">US</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,22 +1,20 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
+// import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import SideBar from '../SideBar/SideBar';
+// import SideBar from '../SideBar/SideBar';
 
 import styles from './MainLayout.module.css';
 
 const MainLayout = () => {
   return (
     <div>
-      <SideBar />
-      <div className={styles.container_suspense}>
-        <Header />
-        <Suspense>
-          <Outlet />
-        </Suspense>
-        <Footer />
-      </div>
+      <Header />
+      <Suspense>
+        <Outlet />
+      </Suspense>
+      {/* <Footer /> */}
     </div>
   );
 };
