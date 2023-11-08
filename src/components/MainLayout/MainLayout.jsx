2df43ff60cb7e8432863d63caffc3a09107863c3
Nazar <npogrebecikyj@gmail.com>
2023-11-08 18:05:27 +0200

    product cards basis

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -2,11 +2,13 @@ import styles from "./MainLayout.module.css";
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import ProductCard from "../ProductCard/ProductCard";
 
 const MainLayout = () => {
    return (
       <div>
          <Header />
+         <ProductCard />
          <Footer />
       </div>
    );
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,13 @@
 import styles from './ProductCard.module.css';
 
+import TestGoods from "../../assets/imges/TestGoods.jpg";
+
 const ProductCard = () => {
    return (
       <div className={styles.product_card}>
-
+         <img src={TestGoods} alt='goods' className={styles.img_product} />
+         <span>Назва</span><br />
+         <span>Ціна</span>
       </div>
    );
 };
