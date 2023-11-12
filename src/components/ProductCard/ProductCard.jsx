946ed60b7546241af48704d73c611a6afa3baf83
Nazar <npogrebecikyj@gmail.com>
2023-11-12 17:29:44 +0200

    slider

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,6 +3,7 @@ import styles from './ProductCard.module.css';
 import TestGoods from "../../assets/imges/TestGoods.jpg";
 
 const ProductCard = () => {
+
    return (
       <div className={styles.product_card}>
          <img src={TestGoods} alt='goods' className={styles.img_product} />
